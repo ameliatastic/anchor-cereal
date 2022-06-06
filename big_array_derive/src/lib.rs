19863fdadebe0265a904cce649f88841fe245893
✨ amelia ✨ <106780815+ameliatastic@users.noreply.github.com>
2022-06-06 13:05:38 -0400

    Way better derive process

--- big_array_derive/src/lib.rs
+++ big_array_derive/src/lib.rs
@@ -1,11 +1,70 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn;
 
-#[proc_macro_derive(NotAnchorSerialize)]
-pub fn nas_derive(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
-}
+#[proc_macro_derive(AbaAnchorDeserializeAnchorSerialize)]
+pub fn abaadas_derive(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
 
-#[proc_macro_derive(NotAnchorDeserialize)]
-pub fn nad_derive(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+    let name = ast.ident;
+    let (ty, len) = match ast.data {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+            ..
+        }) => {
+            if named.len() > 1 { todo!(); }
+
+            match named.first() {
+                Some(syn::Field {
+                    ty: syn::Type::Array(syn::TypeArray { elem, len, .. }),
+                    ..
+                }) => {
+                    (elem.clone(), len.clone())
+                }
+                _ => panic!("Cannot derive")
+            }
+        },
+        _ => panic!("Cannot derive")
+    };
+
+    let gen = quote! {
+        impl BorshDeserialize for #name {
+            #[inline]
+            fn deserialize(buf: &mut &[u8]) -> io::Result<Self> {
+                let mut value = [#ty::default(); #len];
+                if !<#ty>::copy_from_bytes(buf, &mut value)? {
+                    for i in 0..#len {
+                        value[i] = <#ty>::deserialize(buf)?;
+                    }
+                }
+                Ok(#name { value })
+            }
+        }
+
+        impl BorshSerialize for #name {
+            #[inline]
+            fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+                for el in self.value.iter() {
+                    el.serialize(writer)?;
+                }
+                Ok(())
+            }
+        }
+
+        impl Index<usize> for #name {
+            type Output = #ty;
+
+            fn index(&self, idx: usize) -> &Self::Output {
+                &self.value[idx]
+            }
+        }
+
+        impl IndexMut<usize> for #name {
+            fn index_mut(&mut self, idx: usize) -> &mut Self::Output {
+                &mut self.value[idx]
+            }
+        }
+    };
+
+    gen.into()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,13 @@
-pub use std::io::{self, Write};
+pub use std::{
+   io::{self, Write},
+   ops::{Index, IndexMut}
+};
 pub use borsh::{BorshDeserialize, BorshSerialize};
 
-pub trait NotAnchorSerialize {}
-pub trait NotAnchorDeserialize {}
+pub use big_array_derive::*;
 
-#[macro_export]
-macro_rules! def_big_array {
-    ($name:ident, $T:ty, $n:literal) => {
-        //#[derive(Clone, NotAnchorSerialize, NotAnchorDeserialize)]
-        //pub struct $name {
-        //    pub arr: [$T; $n]
-        //}
-        #[derive(Clone)]
-        pub enum $name {
-            Arr([$T; $n])
-        }
-
-        impl BorshDeserialize for $name {
-            #[inline]
-            fn deserialize(buf: &mut &[u8]) -> io::Result<Self> {
-                let mut arr = [<$T>::default(); $n];
-                if !<$T>::copy_from_bytes(buf, &mut arr)? {
-                    for i in 0..$n {
-                        arr[i] = <$T>::deserialize(buf)?;
-                    }
-                }
-                Ok($name::Arr(arr))
-            }
-        }
-
-        impl BorshSerialize for $name {
-            #[inline]
-            fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-                if let $name::Arr(arr) = self {
-                    for el in arr.iter() {
-                        el.serialize(writer)?;
-                    }
-                }
-                Ok(())
-            }
-        }
-    }
-}
+pub trait AbaAnchorDeserializeAnchorSerialize:
+   BorshDeserialize +
+   BorshSerialize +
+   Index<usize> +
+   IndexMut<usize> {}
